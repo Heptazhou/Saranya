164248bdb6881a7dcd5cef0419e95c9dbec1ce37
Heptazhou <heptazhou@icloud.com>
2021-06-30 21:05:13 +0000

    Update

--- make/common/unicode-kind.js
+++ make/common/unicode-kind.js
@@ -2,29 +2,30 @@
 
 exports.isIdeograph = function (c) {
 	return (
+		c === 0x00a9 || //              // Copyright
 		(c >= 0x2e80 && c <= 0x2fff) || // CJK radicals
 		(c >= 0x3192 && c <= 0x319f) || // Ideographic annotation
 		(c >= 0x31c0 && c <= 0x31ef) || // CJK strokes
 		(c >= 0x3400 && c <= 0x4dbf) || // ExtA
 		(c >= 0x4e00 && c <= 0x9fff) || // URO
 		(c >= 0xf900 && c <= 0xfa6f) || // CJK compatibility ideographs
-		(c >= 0x20000 && c <= 0x3ffff) // SIP, TIP
+		(c >= 0x20000 && c <= 0x3ffff) /// SIP, TIP
 	);
 };
 
-exports.isWestern = c => c < 0x2000 || (c >= 0x2070 && c <= 0x218f);
+exports.isWestern = c => (c < 0x2000 && c != 0x00a9) || (c >= 0x2070 && c <= 0x218f);
 
 exports.isKorean = c =>
 	(c >= 0x1100 && c <= 0x11ff) ||
-	(c >= 0xac00 && c <= 0xd7af) ||
 	(c >= 0x3130 && c <= 0x318f) ||
 	(c >= 0x3200 && c <= 0x321e) ||
-	(c >= 0xffa1 && c <= 0xffdc) ||
 	(c >= 0x3260 && c <= 0x327f) ||
 	(c >= 0xa960 && c <= 0xa97f) ||
-	(c >= 0xd7b0 && c <= 0xd7ff);
+	(c >= 0xac00 && c <= 0xd7af) ||
+	(c >= 0xd7b0 && c <= 0xd7ff) ||
+	(c >= 0xffa1 && c <= 0xffdc);
 
-exports.isWS = function (c) {
+exports.isWesternSymbol = function (c) {
 	return (
 		((c >= 0x2000 && c <= 0x200f) || (c >= 0x20a0 && c < 0x3000)) &&
 		!(c >= 0x2e3a && c <= 0x2e3b)

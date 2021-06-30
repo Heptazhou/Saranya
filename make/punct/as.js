76b90bee899b9e8a2bee8ac66307814a8886c44e
Heptazhou <heptazhou@icloud.com>
2021-06-30 21:05:13 +0000

    Update

--- make/punct/as.js
+++ make/punct/as.js
@@ -2,7 +2,12 @@
 
 const introFont = require("../common/intro-font");
 const buildFont = require("../common/build-font");
-const { isWestern, isWS, isLongDash, filterUnicodeRange } = require("../common/unicode-kind");
+const {
+	isWestern,
+	isWesternSymbol,
+	isLongDash,
+	filterUnicodeRange
+} = require("../common/unicode-kind");
 const gc = require("../common/gc");
 
 const { sanitizeSymbols, toPWID } = require("./sanitize-symbols");
@@ -12,17 +17,21 @@ const { transferMonoGeometry, unlinkRefsOfSymbols, populatePwidOfMono } = requir
 module.exports = async function makeFont(argv) {
 	const main = await introFont({ from: argv.main, prefix: "a", ignoreHints: true });
 	const lgc = await introFont({ from: argv.lgc, prefix: "b", ignoreHints: true });
+
 	main.cmap_uvs = null;
 	filterUnicodeRange(
 		main,
-		c => !isWestern(c - 0) && !isLongDash(c - 0, argv.term) && !isWS(c - 0)
+		c =>
+			!isWestern(c - 0, argv.term) &&
+			!isLongDash(c - 0, argv.term) &&
+			!isWesternSymbol(c - 0, argv.term)
 	);
 
 	if (argv.pwid) toPWID(main);
 	if (argv.mono) {
 		unlinkRefsOfSymbols(lgc, argv.term);
-		transferMonoGeometry(main, lgc);
-		populatePwidOfMono(main);
+		transferMonoGeometry(main, lgc, argv.term);
+		populatePwidOfMono(main, argv.term);
 	}
 	if (!argv.pwid) sanitizeSymbols(main, argv.goth, !argv.pwid && !argv.term);
 	if (argv.mono) removeDashCcmp(main, argv.mono);

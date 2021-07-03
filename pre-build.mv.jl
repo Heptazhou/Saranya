4a9505b8547bc52f93a132980edcae4aaa658861
Heptazhou <heptazhou@icloud.com>
2021-07-03 21:54:46 +0000

    Create

---
+++ pre-build.mv.jl
@@ -0,0 +1,22 @@
+function rename()
+	ext = ".ttf"
+	for f in readdir()
+		splitext(f)[2] == ext || continue
+		dst = replace(f, Regex("(?<=[a-z])-(?=italic\\$ext\$)", "i") => "")
+		dst != f && mv(f, dst, force = true)
+	end
+end
+
+try
+	@info "正在重命名"
+	cd(rename, "dist/ttf/")
+	cd(rename, "dist/ttf-hinted/")
+	@info "完成"
+catch e
+	@info "错误"
+	@info e
+end
+length(ARGS) > 0 && exit()
+print("> ")
+readline()
+

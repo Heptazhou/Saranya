5e5c5d0a041c6ef6e7276664488daf4e011ccdf0
Heptazhou <heptazhou@icloud.com>
2021-07-03 21:54:46 +0000

    Create

---
+++ sources/iosevnya-fixed/update.jl
@@ -0,0 +1,38 @@
+const dst = "."
+const src = "../../../Iosevnya/dist/iosevnya/ttf"
+const list = [
+	# "thin"
+	# "thin-italic"
+	# "extralight"
+	# "extralight-italic"
+	# "light"
+	# "light-italic"
+	"regular"
+	"regular-italic"
+	# "medium"
+	# "medium-italic"
+	# "semibold"
+	# "semibold-italic"
+	"bold"
+	"bold-italic"
+	# "extrabold"
+	# "extrabold-italic"
+	# "black"
+	# "black-italic"
+]
+
+try
+	n = 0
+	for f in list .* ".ttf"
+		cp(joinpath(src, "iosevnya-$f"), joinpath(dst, "iosevnya-fixed-$f"), force = true)
+		n += 1
+	end
+	@info "完成 > $n"
+catch e
+	@info "错误"
+	@info e
+end
+length(ARGS) > 0 && exit()
+print("> ")
+readline()
+

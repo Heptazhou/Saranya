566277381d30008c7f0c6f417ed6dece3f18a36c
Heptazhou <heptazhou@icloud.com>
2021-07-03 21:54:46 +0000

    Create

---
+++ build.font.cmd
@@ -0,0 +1,21 @@
+@echo off
+chcp  65001>nul
+cmd/c pnpm up
+echo.
+pause
+echo.
+julia config.update.jl /
+julia pre-build.mv.jl /
+echo.
+pause
+cls
+cmd/c pnpm run build ttf ttc
+echo.
+echo.
+julia post-build.mv.jl /
+julia post-build.clean.jl /
+echo.
+pause
+echo.&echo Start building 7z . . .
+echo.
+build.7z
---
+++ post-build.clean.jl
@@ -0,0 +1,53 @@
+function rmr(path::String)
+	ispath(path) ? (rm(path, recursive = true); 1) : 0
+end
+function rmr(path::Vector{String})
+	sum(rmr.(path))
+end
+
+const list = [
+	# dir
+	# "iosevka-n-fixed-slab"
+	# "iosevka-n-fixed"
+	# "iosevka-n-slab"
+	# "iosevka-n-term-slab"
+	# "iosevka-n-term"
+	# "iosevka-n"
+	# file
+	"shs/SourceHanSans-Bold.otf"
+	"shs/SourceHanSans-Regular.otf"
+	"shs/SourceHanSansHC-Bold.otf"
+	"shs/SourceHanSansHC-Regular.otf"
+	"shs/SourceHanSansHW-Bold.otf"
+	"shs/SourceHanSansHW-Regular.otf"
+	"shs/SourceHanSansHWHC-Bold.otf"
+	"shs/SourceHanSansHWHC-Regular.otf"
+	"shs/SourceHanSansHWK-Bold.otf"
+	"shs/SourceHanSansHWK-Regular.otf"
+	"shs/SourceHanSansHWSC-Bold.otf"
+	"shs/SourceHanSansHWSC-Regular.otf"
+	"shs/SourceHanSansHWTC-Bold.otf"
+	"shs/SourceHanSansHWTC-Regular.otf"
+	"shs/SourceHanSansK-Bold.otf"
+	"shs/SourceHanSansK-Regular.otf"
+	"shs/SourceHanSansSC-Bold.otf"
+	"shs/SourceHanSansSC-Regular.otf"
+	"shs/SourceHanSansTC-Bold.otf"
+	"shs/SourceHanSansTC-Regular.otf"
+]
+
+try
+	@info "正在清理"
+	n = 0
+	for dir in [".build/", "sources/"]
+		n += rmr(dir .* list)
+	end
+	@info "完成 > $n"
+catch e
+	@info "错误"
+	@info e
+end
+length(ARGS) > 0 && exit()
+print("> ")
+readline()
+
